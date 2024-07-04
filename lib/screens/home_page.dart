4aeb5819e4522a7d04d12e13b77087359ed4e2e8
Rohan-450 <zombiecpp106@gmail.com>
2024-07-05 03:37:17 +0530

    UI changed and logo added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/screens/addtask_page.dart';
 import '../constants/colors.dart';
+import '../utils/database.dart';
 import '../utils/todo.dart';
 import '../utils/todo_tile.dart';
 
@@ -13,71 +14,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  //final _myBox = Hive.box('mybox');
-  //ToDoDataBase db =ToDoDataBase();
-
-  final todosList=ToDo.toDoList();
+  List<ToDo> todosList = [];
   List<ToDo> searchList = [];
 
- /* void createInitialData(){
-    ToDo(
-        id: "1",
-        taskName: "Create First Task",
-        taskCompleted: false,
-      );
-  }
-  void loadData(){
-    todosList = _myBox.get('TODOS');
-  }
-  void updateData(){
-    _myBox.put('TODOS', todosList);
-  }*/
+  HiveDB db = HiveDB();
 
   @override
   void initState() {
-    /*if(_myBox.get('TODOS') == null){
-      createInitialData();
-    }else{
-      loadData();
-    }*/
+    todosList = db.getTodos();
     searchList = todosList;
     super.initState();
   }
-  
-  void checkBoxChanged(ToDo todo){
+
+  void checkBoxChanged(ToDo todo) {
     setState(() {
       todo.taskCompleted = !todo.taskCompleted!;
     });
-    //updateData();
+    db.updateTaskDb(todo);
   }
 
-  void deleteTask(String id){
+  void deleteTask(String id) {
     setState(() {
       todosList.removeWhere((item) => item.id == id);
     });
-    //updateData();
+    db.deleteTaskDb(id);
   }
 
-  void searchTask(String query){
+  void searchTask(String query) {
     List<ToDo> results = [];
-    results = todosList.where((element) => element.taskName!.toLowerCase().contains(query.toLowerCase())).toList();
+    results = todosList
+        .where((element) =>
+            element.taskName!.toLowerCase().contains(query.toLowerCase()))
+        .toList();
     setState(() {
       searchList = results;
     });
   }
 
-   void addTask(String toDo) {
-    setState(() {
-      todosList.add(ToDo(
-        id: DateTime.now().microsecondsSinceEpoch.toString(),
-        taskName: toDo,
-      ),
-      );
-    });
-    //updateData();
-  }
+  void addTask(String toDo) {
+    final newTask = ToDo(
+      id: DateTime.now().microsecondsSinceEpoch.toString(),
+      taskName: toDo,
+    );
 
+    setState(() {
+      todosList.add(newTask);
+    });
+
+    db.addTaskDb(newTask);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,16 +71,45 @@ class _HomePageState extends State<HomePage> {
       key: _scaffoldKey,
       appBar: _buildAppBar(),
       drawer: Drawer(
-        child: ListView(
+        child: Column(
           children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: tBlue,
+            Expanded(
+              child: ListView(
+                children: [
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: tBlue,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          'ToDo App',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.edit, color: Colors.white, size: 30,)
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    leading: IconButton(
+                      icon: const Icon(Icons.nights_stay,
+                          color: tBlack, size: 30),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              child: Text('ToDo App', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold,),),
             ),
             ListTile(
-              title: const Text('Made By Rohan Mitra', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ),),
+              title: const Text(
+                'Made By Rohan Mitra🗿',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
               onTap: () {
                 Navigator.pop(context);
               },
@@ -104,106 +118,126 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 0),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(top: 18,),
-              decoration: const BoxDecoration(
-                color: tBgColor,
-              ),
-              child: Column(
-                children:[
-                    searchBox(),
-                    Container(
-                      margin: const EdgeInsets.only(top: 30, bottom: 25,),
-                      child: const Text("All Tasks", 
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                  top: 18,
+                ),
+                decoration: const BoxDecoration(
+                  color: tBgColor,
+                ),
+                child: Column(children: [
+                  searchBox(),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: 30,
+                      bottom: 25,
+                    ),
+                    child: const Text(
+                      "All Tasks",
                       style: TextStyle(
-                        color: tBlack, 
-                        fontSize: 30, 
-                        fontWeight: FontWeight.bold),
+                          color: tBlack,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
                       textAlign: TextAlign.left,
                     ),
-                    ),
-                 ]
-               ),
-            ),
-            
-            Expanded(
-              child: ListView(
-                children:[
-                  for( ToDo todoo in searchList.reversed)
-                    ToDoTile(
-                      todo: todoo,
-                      onChanged: checkBoxChanged,
-                      onDelete: deleteTask,),
-                ]
-              )
-            ),
-        ],)
-      ),
+                  ),
+                ]),
+              ),
+              Expanded(
+                  child: ListView(children: [
+                for (ToDo todoo in searchList.reversed)
+                  ToDoTile(
+                    todo: todoo,
+                    onChanged: checkBoxChanged,
+                    onDelete: deleteTask,
+                  ),
+              ])),
+            ],
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-           var result= await Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddTaskPage()),
-            );
-            if(result!=null){
-              addTask(result);
-            }
-          },
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTaskPage()),
+          );
+          if (result != null) {
+            addTask(result);
+          }
+        },
         backgroundColor: tBlue,
         elevation: 5.0,
         shape: const CircleBorder(),
-        child: const Icon(Icons.add, size:30, color: Colors.white,),
+        child: const Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
       ),
     );
   }
 
-  Widget searchBox(){
+  Widget searchBox() {
     return Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: TextField(
-                  onChanged: (value) => searchTask(value),
-                  decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(Icons.search, color: tBlack,size: 25,),
-                  prefixIconConstraints: BoxConstraints(minWidth: 25, maxHeight: 20),
-                  hintText: " Search",
-                  hintStyle: TextStyle(color: tGrey, fontSize: 20),
-                  border: InputBorder.none,
-                ),
-              ),
-            );
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: TextField(
+        onChanged: (value) => {searchTask(value)},
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tBlack,
+            size: 25,
+          ),
+          prefixIconConstraints: BoxConstraints(minWidth: 25, maxHeight: 20),
+          hintText: " Search",
+          hintStyle: TextStyle(color: tGrey, fontSize: 20),
+          border: InputBorder.none,
+        ),
+      ),
+    );
   }
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   AppBar _buildAppBar() {
     return AppBar(
-      elevation: 0,
-      backgroundColor: tBgColor,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        /*IconButton(icon: Icon(Icons.menu, color: tBlack, size:30,),
+        elevation: 0,
+        backgroundColor: tBgColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            /*IconButton(icon: Icon(Icons.menu, color: tBlack, size:30,),
                 onPressed: (){
                   _scaffoldKey.currentState?.openDrawer();
                 },
           ),*/
-        const Text("ToDo Lists", style: TextStyle(color: tBlack, fontWeight: FontWeight.bold),),
-        Container(
-          height: 40,
-          width: 40,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(0),
-            child: Image.asset("assets/ToDo_icon.png", fit: BoxFit.cover,),
-          )
-        )
-      ],)
-    );
+            const Text(
+              "ToDo Lists",
+              style: TextStyle(color: tBlack, fontWeight: FontWeight.bold),
+            ),
+            Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(0),
+                  child: Image.asset(
+                    "assets/ToDo_icon.png",
+                    fit: BoxFit.cover,
+                  ),
+                ))
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    Hive.box('todoBox').close();
+    super.dispose();
   }
 }
